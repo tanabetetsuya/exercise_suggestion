3298ed931121fc42ad5459679a5c37d423018d21
tanabe tetsuya <tetsumagic123@gmail.com>
2021-08-21 10:12:19 +0000

    [Add] exercise_menu view controller model

--- app/controllers/exercise_menus_controller.rb
+++ app/controllers/exercise_menus_controller.rb
@@ -1,2 +1,48 @@
 class ExerciseMenusController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @comment = Comment.new
+    @comments = Comment.all
+    @user = current_user
+    @exercise_menus = ExerciseMenu.includes(:liked_users).sort{|a,b| b.liked_users.size <=> a.liked_users.size } #いいね数ランキング表示
+  end
+
+  def new
+    @exercise_menu = ExerciseMenu.new
+  end
+
+  def confirmation
+    @exercise_videos = ExerciseVideo.all
+    @exercise_menu = ExerciseMenu.new(exercise_menu_params)
+  end
+
+  def create
+    @exercise_menu = ExerciseMenu.new(exercise_menu_params)
+    @exercise_menu.user_id = current_user.id
+    if @exercise_menu.save
+    @exercise_videos = current_user.exercise_videos.all
+    @exercise_videos.each do |exercise_video|
+      @today_exercises = @exercise_menu.today_exercises.new
+      @today_exercises.title = exercise_video.title
+      @today_exercises.channel_title = exercise_video.channel_title
+      @today_exercises.published_at = exercise_video.published_at
+      @today_exercises.video_id = exercise_video.video_id
+      @today_exercises.save
+      end
+      @exercise_videos = current_user.exercise_videos.destroy_all
+      redirect_to exercise_menus_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @exercise_menu = ExerciseMenu.find(params[:id])
+  end
+
+  private
+  def exercise_menu_params
+    params.require(:exercise_menu).permit(:user_id, :exercise_purpose, :exercise_goal)
+  end
 end
--- app/models/exercise_menu.rb
+++ app/models/exercise_menu.rb
@@ -1,2 +1,14 @@
 class ExerciseMenu < ApplicationRecord
+  has_many :today_exercises, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
+  validates :exercise_purpose, presence: true
+  validates :exercise_goal, presence: true
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
