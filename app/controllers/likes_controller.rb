e515575e24665340d57862b086c973dcf06859d4
tanabe tetsuya <tetsumagic123@gmail.com>
2021-08-21 10:08:36 +0000

    [Add] いいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  def destroy
+    exercise_menu = ExerciseMenu.find(params[:exercise_menu_id])
+    like = current_user.likes.find_by(exercise_menu_id: exercise_menu.id)
+    like.destroy
+    redirect_to exercise_menus_path
+  end
+
+  def create
+    #binding.irb
+    exercise_menu = ExerciseMenu.find(params[:exercise_menu_id])
+    like = current_user.likes.new(exercise_menu_id: exercise_menu.id)
+    like.save
+    redirect_to exercise_menus_path
+  end
 end
