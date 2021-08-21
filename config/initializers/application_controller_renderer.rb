23e2d9c8a9a025202f3006db67cde003137f36a1
tanabe tetsuya <tetsumagic123@gmail.com>
2021-08-21 10:24:36 +0000

    [Add] Refile.secret_keyの追記

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '91cf7cd43562679a337236dfad7a3f0c5cd8f52f67edc99fba1f1eca23696fafc32c36a911d21d8d0d7f3e50962ffa6c5b6a9163a5d5b1201a81b8f01e768e9b'
