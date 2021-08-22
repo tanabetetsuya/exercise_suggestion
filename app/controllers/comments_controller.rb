ca4c3788c69d49f44f4e29ab74c869768a962493
tanabe tetsuya <tetsumagic123@gmail.com>
2021-08-22 02:31:28 +0000

    [Add] comment controllerの作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    #binding.irb
+    exercise_menu = ExerciseMenu.find(params[:exercise_menu_id])
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    comment.exercise_menu_id = exercise_menu.id
+    comment.save
+    redirect_to exercise_menu_comments_path(exercise_menu.id)
+  end
+
+  def destroy
   end
 
   def index
+    @exercise_menu = ExerciseMenu.find(params[:exercise_menu_id])
+    @comments = @exercise_menu.comments.all.page(params[:page]).per(5)
+    @comment = Comment.new
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :exercise_menu_id, :comment)
   end
 end
